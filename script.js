fdda8b0bab7ebbab1ceb8eb2d6808cd127c9ba7c
Angelise Perez <angelisep3@nycstudents.net>
2026-05-15 12:39:48 -0400

    console

--- script.js
+++ script.js
@@ -19,4 +19,22 @@ divide () {
 };
 
 let math = new MathFunctions(10, 5);
-console.log()
+console.log(math.add());
+console.log(math.subtract());
+console.log(math.multiply());
+console.log(math.divide());
+
+class Intro {
+    constructor (name) {
+        this.name = name;
+    }
+}
+
+english () {
+    return "Hello" + this.name;
+}
+
+english () {
+    return "Hello" + this.name;
+}
+
