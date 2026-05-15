348042846df969f41bc754aede759626fcf37449
Angelise Perez <angelisep3@nycstudents.net>
2026-05-15 12:37:13 -0400

    class

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function add(MathFunctions) {
+class MathFunctions {
+constructor(a, b) {
+    this.a = a;
+    this.b = b;
+} 
 
+add () {
+    return this.a +this.b;
 }
+
+subtract () {
+    return this.a * this,b;
+}
+
+divide () {
+    return this.a / this.b;
+}
+
+};
+
+let math = new MathFunctions(10, 5);
+console.log()
